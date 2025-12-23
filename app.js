e117efc9160eaa68774c530bcf82f7010719a373
Julia Beatriz <cjuliabeatriz@gmail.com>
2025-12-23 15:34:01 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function ReceberNomeComoParametro (numero) { 
+    alert ('Oii, tudo bem com vc?')
+ let nomeUsuario = prompt ('Qual seu nome?') 
+ alert ('olá  ' + nomeUsuario)
+ NumeroEmDobro()
+}
+function NumeroEmDobro (dobro) {
+  let numeroColetado = prompt ('digite qualquer número para vermos quanto ele é em dobro: ')
+  alert ('seu número em dobro vai ser igual a; ' + numeroColetado * 2)
+}
