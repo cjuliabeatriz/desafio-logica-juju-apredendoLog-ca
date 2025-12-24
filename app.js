3c9286a06f87153be6973dab3918c2661bf5b6e5
Julia Beatriz <cjuliabeatriz@gmail.com>
2025-12-24 19:40:32 -0300

    finalização de desafio.
    Atualização. Todos os desafios feitos; 
    
    Criar uma função que exibe "Olá, mundo!" no console.
    
    Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
    
    Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
    
    Criar uma função que recebe três números como parâmetros e retorna a média deles.
    
    Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
    
    Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo

--- app.js
+++ app.js
@@ -6,7 +6,23 @@ function ReceberNomeComoParametro (numero) {
  NumeroEmDobro()
 }
 function NumeroEmDobro (dobro) {
-  let numeroColetado = prompt ('digite qualquer número para vermos quanto ele é em dobro ')
+  let numeroColetado = prompt ('digite qualquer número para vermos quanto ele é em dobro: ')
   alert ('seu número em dobro vai ser igual a; ' + numeroColetado * 2)
+  CalcularMedia ()
 }
-
+function CalcularMedia () {
+  alert ('vamos calcular algumas médias :)')
+  let n1 = parseFloat(prompt('Primeiro número da média;  '))
+  let n2 = parseFloat(prompt('segundo número da média;'))
+  let n3 = parseFloat(prompt('terceiro número da média;')) 
+  let ResultadoDaMedia = (n1 + n2 + n3) / 3 
+  let NumeroMaior = Math.max(n1, n2, n3)
+  let MaiorNumero = NumeroMaior * NumeroMaior
+  alert (
+    '   RELATORIO FINAL-------------                 ' + 
+    '   RESULTADO DA MÉDIA:  ' +   ResultadoDaMedia     +
+    '   RESULTADO DO NÚMERO MAIOR:   ' +   NumeroMaior     + 
+    '  MULTIPLICAÇÃO DO MAIOR NÚMERO:     ' +   MaiorNumero 
+    
+  )
+}
